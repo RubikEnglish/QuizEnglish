a2d5ed024aa5568cc2620347c358ecc082e0de24
Jhonatan Vera <jveraolmos@gmail.com>
2021-10-11 01:38:26 -0500

    first commit
    first commit

---
+++ data/data.js
@@ -0,0 +1,37 @@
+export const data = [
+
+    {
+        question: 'Cual es el verbo que significa ser o estar en ingles?',
+        choices: [
+            'To be',
+            'To have',
+            'To read',
+            'To eat'
+        ],
+        answer: 'To be',
+    },
+
+    {
+        question: 'Cual es el verbo que significa haber o tener?',
+        choices: [
+            'To be',
+            'To have',
+            'To do',
+            'To read'
+        ],
+        answer: 'To have',
+    },
+
+    {
+        question: 'Cual es el verbo que significa hacer?',
+        choices: [
+            'To be',
+            'To have',
+            'To do',
+            'To eat',
+            'To swimm'
+        ],
+        answer: 'To do',
+    },
+
+];
